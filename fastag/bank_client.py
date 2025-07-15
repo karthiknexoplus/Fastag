05cd0d2016796ad854f0eb8e769da45c48b116ba
karthikdevarajiit <karthik@nexoplus.in>
2025-07-16 01:44:34 +0530

    Update private key and certificate paths to certs/ directory in project root

--- fastag/bank_client.py
+++ fastag/bank_client.py
@@ -13,8 +13,8 @@ PROD_URL = os.getenv('BANK_API_PROD_URL', 'https://prod-bank-url.example.com/syn
 # Choose environment: 'UAT' or 'PROD'
 BANK_ENV = os.getenv('BANK_API_ENV', 'UAT')
 
-PRIVATE_KEY_PATH = "/home/ubuntu/certs/fastag.onebee.in/rsa-privkey.pem"
-CERT_PATH = "/home/ubuntu/certs/fastag.onebee.in/rsa-cert.pem"
+PRIVATE_KEY_PATH = "certs/rsa-privkey.pem"
+CERT_PATH = "certs/rsa-cert.pem"
 
 
 def get_bank_url():
