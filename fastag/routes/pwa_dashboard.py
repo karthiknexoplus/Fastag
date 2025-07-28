42a464ea4dceecc8cb3c18b1d0dcd80139cbcea1
karthikdevarajiit <karthik@nexoplus.in>
2025-07-28 15:22:40 +0530

    Fix PWA dashboard route to '/' for correct url_prefix handling

--- fastag/routes/pwa_dashboard.py
+++ fastag/routes/pwa_dashboard.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 pwa_dashboard_bp = Blueprint('pwa_dashboard', __name__)
 
-@pwa_dashboard_bp.route('/pwa-dashboard')
+@pwa_dashboard_bp.route('/')
 def pwa_dashboard():
     return render_template('pwa_dashboard_cards.html') 
