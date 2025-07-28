5ee76e8169e4de5208b8010b7a5234e136e6950d
karthikdevarajiit <karthik@nexoplus.in>
2025-07-28 15:24:24 +0530

    Update PWA dashboard route to include strict_slashes=False for improved URL handling

--- fastag/routes/pwa_dashboard.py
+++ fastag/routes/pwa_dashboard.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 pwa_dashboard_bp = Blueprint('pwa_dashboard', __name__)
 
-@pwa_dashboard_bp.route('/')
+@pwa_dashboard_bp.route('/', strict_slashes=False)
 def pwa_dashboard():
     return render_template('pwa_dashboard_cards.html') 
